41ab07c32cbf6a1d90cdf228e422afb47af514d5
Siddharth S <siddharths.mail06@gmail.com>
2026-05-04 20:38:41 +0530

    Create Book.java

---
+++ src/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    private String bookId;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String bookId, String title,String author,boolean isAvailable) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+    public String getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+}
